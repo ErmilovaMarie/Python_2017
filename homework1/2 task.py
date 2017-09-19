0bc0384dbc748c90507ad291462ec0aa0c4c37af
Ermilova Marie <ermilova.m98@mail.ru>
2017-09-20 00:12:44 +0300

    Second task

--- homework1/2 task.py
+++ homework1/2 task.py
@@ -2,9 +2,9 @@
 a = float(input('Введите первое число:'))
 b = float(input('Введите второе число:'))
 
- symbol = input('Ведите знак:')
+symbol = input('Введите знак:')
  
- if (symbol == '+'):
+if (symbol == '+'):
 ...     print ('Сумма равна: ', a + b)
 ... elif (symbol == '-'):
 ...     print ('Разность равна: ', a-b)
