bcd62ba1cfa3775aa6e77a1240a38f195d9d7f5f
Ermilova Marie <ermilova.m98@mail.ru>
2017-09-20 21:25:08 +0300

    Third task

---
+++ homework1/3 task.py
@@ -0,0 +1,9 @@
+﻿n = int(input("Введите число n = "))
+list = []
+for i in range(2, n+1):
+    for j in list:
+        if i % j == 0:
+            break
+    else:
+        list.append(i)
+print (list)
